228edae33c53ee7f3fae208c475f00cd73432a35
DHCGamer1055 <dharries2003@gmail.com>
2021-07-11 21:01:53 +0100

    ae1

--- rblx/projects/js/stopwatch.js
+++ rblx/projects/js/stopwatch.js
@@ -6,6 +6,7 @@ const btn_reset = document.getElementById('reset')
 var Interval;
 var Seconds = 0;
 var MinSeconds = 0;
+var HourSeconds = 0;
 
 btn_start.onclick = function() {
     clearInterval(Interval)
@@ -18,17 +19,21 @@ btn_stop.onclick = function() {
 
 btn_reset.onclick = function() {
     clearInterval(Interval);
-    Seconds = 0; MinSeconds = 0;
+    Seconds = 0; MinSeconds = 0; HourSeconds = 0;
     StopWatch.innerHTML = Seconds + "s";
 }
 
 Timer = function() {
-    Seconds += 1
-    MinSeconds += 1
+    Seconds += 1; MinSeconds += 1; HourSeconds += 1;
 
     if (Seconds >= 60) {
         Seconds = 0
     }
+    if ((MinSeconds/60) >= 60) {
+        MinSeconds = 0
+    }
 
-    StopWatch.innerHTML = Math.floor(MinSeconds/60) + "m " + Seconds + "s"
+    var Mins = Math.floor(MinSeconds/60);
+    var Hours = Math.floor(HourSeconds/3600);
+    StopWatch.innerHTML = Hours + "h" + Mins + "m " + Seconds + "s"
 }
