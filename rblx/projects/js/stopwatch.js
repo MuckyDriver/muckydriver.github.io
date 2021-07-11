5424db2e67051c822277c69109a3ffb6e632d865
DHCGamer1055 <dharries2003@gmail.com>
2021-07-11 22:55:35 +0100

    Update stopwatch.js

--- rblx/projects/js/stopwatch.js
+++ rblx/projects/js/stopwatch.js
@@ -4,34 +4,28 @@ const btn_stop = document.getElementById('stop')
 const btn_reset = document.getElementById('reset')
 
 var Interval;
-var Seconds = 0;
-var MinSeconds = 0;
-var HourSeconds = 0;
+var Seconds = 0; var MinSeconds = 0; var HourSeconds = 0;
 
-btn_start.onclick = function() {
-    clearInterval(Interval)
-    Interval = setInterval(Timer, 1000)
+btn_start.onclick = function() { 
+    clearInterval(Interval); 
+    Interval = setInterval(Timer, 1000) 
 }
 
-btn_stop.onclick = function() {
-    clearInterval(Interval)
+btn_stop.onclick = function() { 
+    clearInterval(Interval) 
 }
 
-btn_reset.onclick = function() {
-    clearInterval(Interval);
-    Seconds = 0; MinSeconds = 0; HourSeconds = 0;
-    StopWatch.innerHTML = "0h 0m 0s";
+btn_reset.onclick = function() { 
+    clearInterval(Interval); 
+    Seconds = 0; MinSeconds = 0; HourSeconds = 0; 
+    StopWatch.innerHTML = "0h 0m 0s"; 
 }
 
 Timer = function() {
     Seconds += 1; MinSeconds += 1; HourSeconds += 1;
 
-    if (Seconds >= 60) {
-        Seconds = 0
-    }
-    if ((MinSeconds/60) >= 60) {
-        MinSeconds = 0
-    }
+    if (Seconds >= 60) { Seconds = 0 }
+    if ((MinSeconds/60) >= 60) { MinSeconds = 0 }
 
     var Mins = Math.floor(MinSeconds/60);
     var Hours = Math.floor(HourSeconds/3600);
