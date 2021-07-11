ac121d43c686c4fe9e795550eae3648ad78a5682
DHCGamer1055 <dharries2003@gmail.com>
2021-07-11 13:58:02 +0100

    Update LocalClockScript.js

--- js/LocalClockScript.js
+++ js/LocalClockScript.js
@@ -1,7 +1,6 @@
 // Constants
 const ClockItem = document.getElementById("user-clock-local")
 const Msg = 'since on this page!'
-const CHTML = ClockItem.innerHTML
 
 // Variables
 var count = 0;
@@ -11,8 +10,8 @@ var waitTime = 1000 // ms // start timeset
 setInterval(function() {
     count += 1
     if (count < 60) { 
-        CHTML = count + "s " + Msg 
+        ClockItem.innerHTML = count + "s " + Msg 
     } else { 
-        CHTML = Math.floor(count/60) + "m " + Msg 
+        ClockItem.innerHTML = Math.floor(count/60) + "m " + Msg 
     }
 }, waitTime)
