9730a9c8b6e0d7912c6248d44a7e632ea81fabec
DHCGamer1055 <dharries2003@gmail.com>
2021-07-12 13:13:16 +0100

    Update timer.js

--- rblx/projects/js/timer.js
+++ rblx/projects/js/timer.js
@@ -8,32 +8,26 @@ var Interval;
 var Seconds;
 
 btn_start.onclick = function() { 
-    var content = Number(input.value)
-    Seconds = content + 1
+    Seconds = Number(input.value) + 1
     clearInterval(Interval); 
     TimerElement.style.color = 'rgb(0,0,0)'
     Interval = setInterval(Timer, 1000) 
 }
 
-btn_stop.onclick = function() { 
-    clearInterval(Interval) 
-}
+btn_stop.onclick = function() { clearInterval(Interval) }
 
 btn_reset.onclick = function() { 
     clearInterval(Interval); 
-    Seconds = 0;
-    input.value = ''
+    Seconds = 0; input.value = ''
     TimerElement.style.color = 'rgb(0,0,0)'
     TimerElement.innerHTML = "Ready!"; 
 }
 
 Timer = function() {
     Seconds -= 1;
-
     if (Seconds <= 0) {
         TimerElement.style.color = 'rgb(200,0,0)';
         TimerElement.innerHTML = "Finish!";
-
         clearInterval(Interval)
     } else {
         TimerElement.innerHTML = Seconds + "s left"
