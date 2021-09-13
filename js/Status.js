520f9e7743c78f439474b1a90bc2732fd6777ffe
DHCGamer1055 <dharries2003@gmail.com>
2021-09-13 13:04:37 +0100

    Update Status.js

--- js/Status.js
+++ js/Status.js
@@ -2,6 +2,6 @@ var Request = new XMLHttpRequest();
 Request.open("GET","./json/status.json", false);
 Request.send(null);
 
-var StatusData = JSON.parse(request.responseText);
+var StatusData = JSON.parse(Request.responseText);
 
 console.log(StatusData)
