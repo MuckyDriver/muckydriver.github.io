74f1458c403a7b3a31b28270d0cc0979d4408591
DHCGamer1055 <dharries2003@gmail.com>
2021-09-13 13:22:58 +0100

    df

--- js/Status.js
+++ js/Status.js
@@ -5,12 +5,5 @@ Request.send(null);
 var StatusData = JSON.parse(Request.responseText);
 
 // Applying the data to the current page.
-console.log(StatusData.Version)
-
 const Header = document.getElementById('json-status-header')
-
 Header.innerHTML = StatusData.Status;
-
-var Progress = document.createElement('progress')
-
-Progress.appendChild(Header)
