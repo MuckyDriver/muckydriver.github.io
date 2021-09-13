bfa870697af17372513227fa3b7785694f9233fb
DHCGamer1055 <dharries2003@gmail.com>
2021-09-13 14:02:49 +0100

    Update Status.js

--- js/Status.js
+++ js/Status.js
@@ -5,11 +5,13 @@ Request.send(null);
 var StatusData = JSON.parse(Request.responseText);
 
 // Applying the data to the current page.
-if (window.location.pathname = '/status.html') {
+if (window.location.pathname = 'status.html') {
     const Header = document.getElementById('json-status-header')
 
     Header.style.color = '#4ba823';
     Header.innerHTML = StatusData.Status;
+} else {
+    return
 }
 
 // This is for all pages which want to get the current game version, plus other data packages
