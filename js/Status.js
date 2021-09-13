7d8cdab36bdc3870180404960c61c7b3b1636f62
DHCGamer1055 <dharries2003@gmail.com>
2021-09-13 13:15:44 +0100

    Update Status.js

--- js/Status.js
+++ js/Status.js
@@ -10,3 +10,7 @@ console.log(StatusData.Version)
 const Header = document.getElementById('json-status-header')
 
 Header.innerHTML = StatusData.Status;
+
+var Progress = document.createElement('progress')
+
+Progress.parentElement = Header.innerHTML
