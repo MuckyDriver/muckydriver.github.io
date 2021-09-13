4edab1c760732bfa1859b064b20b68d54e87a66c
DHCGamer1055 <dharries2003@gmail.com>
2021-09-13 14:06:05 +0100

    Update Status.js

--- js/Status.js
+++ js/Status.js
@@ -5,7 +5,7 @@ Request.send(null);
 var StatusData = JSON.parse(Request.responseText);
 
 // Applying the data to the current page.
-if (window.location.pathname = 'status.html') {
+if (window.location.pathname.split("/").pop() == 'status' || window.location.pathname.split("/").pop() == 'status.html') {
     const Header = document.getElementById('json-status-header')
 
     Header.style.color = '#4ba823';
