f04da761bdb8e87d84e60ee79b9a8a2952074f39
DHCGamer1055 <dharries2003@gmail.com>
2021-09-13 13:10:39 +0100

    A

--- js/Status.js
+++ js/Status.js
@@ -1,7 +1,12 @@
-
+// Getting the Json Data: Status.json
 var Request = new XMLHttpRequest();
 Request.open("GET","./json/status.json", false);
 Request.send(null);
 var StatusData = JSON.parse(Request.responseText);
 
+// Applying the data to the current page.
 console.log(StatusData.Version)
+
+const Header = document.getElementById('json-status-header')
+
+Header.innerText = StatusData.Status;
