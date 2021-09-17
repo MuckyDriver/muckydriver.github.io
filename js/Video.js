f5d3240ad880fbbcf623808ce93f36d80746756b
DHCGamer1055 <dharries2003@gmail.com>
2021-09-17 16:29:44 +0100

    SAMisAMAZING

--- js/Video.js
+++ js/Video.js
@@ -2,6 +2,7 @@ const closer = document.getElementById('close')
 closer.onclick = function() { window.location = 'https://muckydriver.net/' }
 
 const videoSelect = document.getElementById("video-select")
+const videoFormatSelect = document.getElementById('video-format')
 const body = document.body;
 
 var videos = {
@@ -10,19 +11,20 @@ var videos = {
     ['Truck Mesh Preview']: 'files/truck.mp4'
 }
 
+var vidFormat = 'mp4'
 videoSelect.onchange = function() {
     if (videos[videoSelect.value]) {
         const video = document.getElementById("video")
-        video.remove()
-
-        var url = videos[videoSelect.value];
         const videoClone = document.createElement('video')
-        videoClone.id = 'video'; videoClone.controls = true
-
         const newVid = document.createElement('source');
-        newVid.src = url; newVid.type = 'video/mp4'; newVid.id = 'video-source';
+        const url = videos[videoSelect.value];
 
+        video.remove()
+        videoClone.id = 'video'; videoClone.controls = true
+        newVid.src = url; newVid.type = 'video/'+vidFormat; newVid.id = 'video-source';
         body.appendChild(videoClone)
         videoClone.appendChild(newVid)
     }
 }
+
+videoFormatSelect.onchange = function() { vidFormat = videoFormatSelect.value }
