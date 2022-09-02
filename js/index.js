5dca628b549c07e7d04721bd9df8e76ce4133f69
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-09-02 11:57:48 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -32,23 +32,21 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 } 
 
 function closeSlideshow() {
     slideshowcontainer.style.display = 'none'
+    window.location.hash = "slideshow:hidden"
 }
 
 function openSlideshow() {
     slideshowcontainer.style.display = 'block'
+    window.location.hash = 'slideshow:all'
 }
